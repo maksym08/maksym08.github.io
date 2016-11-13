6095387247c9d200572d1cde99f5127e3bfa862f
maksym08 <maksym08@gmail.com>
2016-11-13 12:23:59 +0200

    Styles refactoring

--- js/js.js
+++ js/js.js
@@ -4,22 +4,51 @@
 angular.module('app', []).controller('iconsCtrl', function ($scope, $http) {
 
     $scope.images = [];
+    $scope.fireTypes = [];
     $scope.visibVal = true;
     $scope.loaded = false;
     $scope.loadedMore = true;
     var count = 0;
     var limit = 12;
 
-    $http.get('https://pokeapi.co/api/v1/pokemon/?limit=12')
+    $http.get('https://pokeapi.co/api/v1/type/?limit=999')
         .then(function (response) {
-            for (var i = 0; i < limit; ++i) {
-                $scope.images[i] = [response.data.objects[i].national_id, response.data.objects[i].types, response.data.objects[i].name];
+            for (var i = 0; i < response.data.objects.length; ++i) {
+                $scope.fireTypes[i] = [response.data.objects[i].name, getRamdomColor()];
             }
-            $scope.loaded = true;
+
+            $http.get('https://pokeapi.co/api/v1/pokemon/?limit=12')
+                .then(function (response) {
+                    for (var i = 0; i < limit; ++i) {
+                        $scope.images[i] = [response.data.objects[i].national_id, response.data.objects[i].types, response.data.objects[i].name];
+                        for(var j=0; j< $scope.fireTypes.length; ++j) {
+                            console.log($scope.fireTypes[j][0].toLowerCase(),  response.data.objects[i].types[0].name);
+                            if($scope.fireTypes[j][0].toLowerCase() === response.data.objects[i].types[0].name) {
+                                $scope.images[i][1][0].color = $scope.fireTypes[j][1];
+                            }
+                            if(response.data.objects[i].types[1] && $scope.fireTypes[j][0].toLowerCase() === response.data.objects[i].types[1].name) {
+                                $scope.images[i][1][1].color = $scope.fireTypes[j][1];
+                            }
+                        }
+                    }
+                    $scope.loaded = true;
+                })
+                .catch(function () {
+                    console.error("GET JSON ERROR");
+                });
         })
         .catch(function () {
             console.error("GET JSON ERROR");
         });
+
+    var getRamdomColor = function() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
 
     $scope.loadMoreClick = function () {
         ++count;
